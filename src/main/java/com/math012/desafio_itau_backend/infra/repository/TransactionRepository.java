48b892823ed9c830cb735cadbfceefe48e588c3d
Math012 <math111.0811@gmail.com>
2025-08-05 14:33:41 -0300

    Adicionando o repository: TransactionRepository para simular as operações do banco de dados envolvendo as transações

---
+++ src/main/java/com/math012/desafio_itau_backend/infra/repository/TransactionRepository.java
@@ -0,0 +1,16 @@
+package com.math012.desafio_itau_backend.infra.repository;
+
+import com.math012.desafio_itau_backend.infra.entity.TransactionEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository {
+
+    TransactionEntity saveTransaction(TransactionEntity transactionEntity);
+
+    void deleteAll();
+
+    List<TransactionEntity> findByTime();
+}
