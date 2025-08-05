2865faa9357ba4d014b4811f557be670612e2c34
Math012 <math111.0811@gmail.com>
2025-08-05 14:54:12 -0300

    Endpoint para excluir todas as transações.

--- src/main/java/com/math012/desafio_itau_backend/controller/TransactionController.java
+++ src/main/java/com/math012/desafio_itau_backend/controller/TransactionController.java
@@ -5,10 +5,7 @@ import com.math012.desafio_itau_backend.business.dto.request.TransactionRequestD
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -21,5 +18,11 @@ public class TransactionController {
     public ResponseEntity<Void> saveTransaction(@RequestBody TransactionRequestDTO request){
         service.saveTransaction(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/transacao")
+    public ResponseEntity<Void> deleteAll(){
+        service.deleteAll();
+        return ResponseEntity.ok().build();
     }
 }
