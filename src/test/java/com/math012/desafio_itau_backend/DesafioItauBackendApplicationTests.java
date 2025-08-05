e2def1d1c6c267d7740f93604383b1038dceeb46
Math012 <math111.0811@gmail.com>
2025-08-05 06:37:16 -0300

    Primeiro commit

---
+++ src/test/java/com/math012/desafio_itau_backend/DesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.desafio_itau_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
