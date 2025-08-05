d73e89b942dea78bc1d116b757c1baf1dce35f87
Math012 <math111.0811@gmail.com>
2025-08-05 14:48:54 -0300

    Adicionando o método para a criação da transação.

--- src/main/java/com/math012/desafio_itau_backend/business/TransactionService.java
+++ src/main/java/com/math012/desafio_itau_backend/business/TransactionService.java
@@ -2,13 +2,17 @@ package com.math012.desafio_itau_backend.business;
 
 import com.math012.desafio_itau_backend.business.converter.TransactionConverter;
 import com.math012.desafio_itau_backend.business.dto.request.TransactionRequestDTO;
+import com.math012.desafio_itau_backend.infra.entity.TransactionEntity;
+import com.math012.desafio_itau_backend.infra.exception.RequestInvalidException;
 import com.math012.desafio_itau_backend.infra.exception.TransactionDateException;
 import com.math012.desafio_itau_backend.infra.exception.TransactionValueException;
 import com.math012.desafio_itau_backend.infra.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 @AllArgsConstructor
 @Service
@@ -17,6 +21,19 @@ public class TransactionService {
     private final TransactionConverter converter;
 
     private final TransactionRepository repository;
+
+    public void saveTransaction(TransactionRequestDTO dto){
+        try {
+            verifyTransaction(dto);
+            TransactionEntity entity = converter.forTransactionEntityFromTransactionRequestDTO(dto);
+            entity.setId(1L);
+            Instant instant = entity.getDataHora().toInstant();
+            entity.setDataHora(instant.atOffset(ZoneOffset.ofHours(-3)));
+            repository.saveTransaction(entity);
+        }catch (NullPointerException e){
+            throw new RequestInvalidException("Erro ao registrar a transação: campos inválidos!");
+        }
+    }
 
     public void verifyTransaction(TransactionRequestDTO dto){
         if (!dto.getDataHora().isBefore(OffsetDateTime.now())){
